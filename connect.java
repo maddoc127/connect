0274a6df8ac5ee6bd7152353185b1730c86286bb
Deniz Mirza <denizimmirza@gmail.com>
2025-03-06 01:14:58 +0300

    connect four v1

---
+++ connect.java
@@ -0,0 +1,20 @@
+public class connect {
+
+public static void printboard(String board[][]){
+    for(int i=1;i<(board.length);++i){
+        for(int j=1;j<(board.length);++j){
+            System.out.print(" B "); 
+}
+System.out.println(""); 
+}}
+
+public static void dropDisc(String player, String board[][]){
+    
+}
+
+    public static void main(String[] args){
+    int row = 7; 
+    int column = 8; 
+    String board[][] = new String[row][column];
+    printboard(board);
+}}
